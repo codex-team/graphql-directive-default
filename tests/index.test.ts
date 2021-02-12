5c8e711da01b543ff54b55ea066d065681e37bb4
Nikita Melnikov <nikmel2803@gmail.com>
2021-02-12 16:36:54 +0300

    implement basic test-cases

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,172 @@
-import main from "../src";
+import {makeExecutableSchema} from "@graphql-tools/schema";
+import createDirectiveDefault from "../src";
+import { graphql } from "graphql";
 
-describe('TypeScript library template', () => {
-  test('Should return correct result', () => {
-    expect(main()).toBe('It works!')
+describe('@default directive', () => {
+  test('Should return default value if parent value is null or undefined', async() => {
+    const schema = makeExecutableSchema({
+      typeDefs: `
+        directive @default(value: String!) on FIELD_DEFINITION
+
+        type Query {
+          nullValue: String! @default(value: "default value for null")
+          undefinedValue: String! @default(value: "default value for undefined")
+        }
+      `,
+      resolvers: {
+        Query: {
+          nullValue: () => null,
+          undefinedValue: () => null
+        }
+      },
+      schemaTransforms: [
+        createDirectiveDefault().schemaTransformer
+      ]
+    });
+
+    const { data, errors } = await graphql(
+      schema,
+        `
+        query {
+          nullValue
+          undefinedValue
+        }
+      `,
+    );
+
+    expect(data?.nullValue).toBe('default value for null');
+    expect(data?.undefinedValue).toBe('default value for undefined');
+    expect(errors).toBeUndefined();
+  })
+
+  test('Shouldn`t return default value if value is defined', async() => {
+    const schema = makeExecutableSchema({
+      typeDefs: `
+        directive @default(value: String!) on FIELD_DEFINITION
+
+        type Query {
+          value: String! @default(value: "default value")
+        }
+      `,
+      resolvers: {
+        Query: {
+          value: () => 'val',
+        }
+      },
+      schemaTransforms: [
+        createDirectiveDefault().schemaTransformer
+      ]
+    });
+
+    const { data, errors } = await graphql(
+      schema,
+        `
+        query {
+          value
+        }
+      `,
+    );
+
+    expect(data?.value).toBe('val');
+    expect(errors).toBeUndefined();
+  })
+
+  test('Should return boolean default value', async() => {
+    const schema = makeExecutableSchema({
+      typeDefs: `
+        directive @default(value: String!) on FIELD_DEFINITION
+
+        type Query {
+          value: Boolean! @default(value: "true")
+        }
+      `,
+      resolvers: {
+        Query: {
+          value: () => null,
+        }
+      },
+      schemaTransforms: [
+        createDirectiveDefault().schemaTransformer
+      ]
+    });
+
+    const { data, errors } = await graphql(
+      schema,
+        `
+        query {
+          value
+        }
+      `,
+    );
+
+    expect(data?.value).toBe(true);
+    expect(errors).toBeUndefined();
+  })
+
+  test('Should return number default value', async() => {
+    const schema = makeExecutableSchema({
+      typeDefs: `
+        directive @default(value: String!) on FIELD_DEFINITION
+
+        type Query {
+          value: Int! @default(value: "123")
+        }
+      `,
+      resolvers: {
+        Query: {
+          value: () => null,
+        }
+      },
+      schemaTransforms: [
+        createDirectiveDefault().schemaTransformer
+      ]
+    });
+
+    const { data, errors } = await graphql(
+      schema,
+      `
+        query {
+          value
+        }
+      `,
+    );
+
+    expect(data?.value).toBe(123);
+    expect(errors).toBeUndefined();
+  })
+
+  test('Should return string default value even is provided default value parses as boolean or number', async() => {
+    const schema = makeExecutableSchema({
+      typeDefs: `
+        directive @default(value: String!) on FIELD_DEFINITION
+
+        type Query {
+          valueInt: String! @default(value: "123")
+          valueBoolean: String! @default(value: "true")
+        }
+      `,
+      resolvers: {
+        Query: {
+          valueInt: () => null,
+          valueBoolean: () => null,
+        }
+      },
+      schemaTransforms: [
+        createDirectiveDefault().schemaTransformer
+      ]
+    });
+
+    const { data, errors } = await graphql(
+      schema,
+      `
+        query {
+          value
+        }
+      `,
+    );
+
+    expect(data?.value).toBe('123');
+    expect(data?.value).toBe('true');
+    expect(errors).toBeUndefined();
   })
 });
